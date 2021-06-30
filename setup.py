7968636175ac104487d1c8334089226832f71b4e
Ciaran Doherty <cad106uk@gmail.com>
2021-06-30 12:25:04 +0100

    First example (with REAME docs) for releasing Python code.

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="just-python-firebreak",
+    version="0.0.1",
+    author="DIT",
+    author_email="ciaran.doherty@digital.trade.gov.uk",
+    description="A firebreak learning project",
+    packages=find_packages(),
+)
