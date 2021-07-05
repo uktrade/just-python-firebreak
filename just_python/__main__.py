dce966b164944e0e164d152b834cb314f7eb701f
Ciaran Doherty <cad106uk@gmail.com>
2021-07-05 19:07:06 +0100

    Move db config from __main__ to main function

--- just_python/__main__.py
+++ just_python/__main__.py
@@ -1,9 +1,7 @@
 from aiohttp import web
 
 from . import main
-from .db import configure_db
 
 if __name__ == "__main__":
-    configure_db()
     app = main()
     web.run_app(app)
--- just_python/main.py
+++ just_python/main.py
@@ -1,6 +1,8 @@
+from .db import configure_db
 from .server import configure_http_server
 
 
 def main():
     app = configure_http_server()
+    configure_db()
     return app
