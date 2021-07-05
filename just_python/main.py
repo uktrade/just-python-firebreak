152891be73b066ac44998c6f493e68c319b9dd79
Ciaran Doherty <cad106uk@gmail.com>
2021-07-05 10:57:43 +0100

    Adding websocket to the backend

--- just_python/main.py
+++ just_python/main.py
@@ -1,3 +1,4 @@
+import aiohttp
 from aiohttp import web
 
 
@@ -5,9 +6,34 @@ async def handle(request):
     name = request.match_info.get("name", "Anonymous")
     text = "Hello, " + name
     return web.Response(text=text)
+async def websocket_handler(request):
+    print("Websocket connection starting")
+    ws = aiohttp.web.WebSocketResponse()
+    await ws.prepare(request)
+    print("Websocket connection ready")
+
+    async for msg in ws:
+        print(msg)
+        if msg.type == aiohttp.WSMsgType.TEXT:
+            print(msg.data)
+            if msg.data == "close":
+                print("CLOSE")
+                await ws.close()
+            else:
+                print("SEND")
+                await ws.send_str(msg.data + "/answer")
+
+    print("Websocket connection closed")
+    return ws
 
 
 def main():
     app = web.Application()
-    app.add_routes([web.get("/", handle), web.get("/{name}", handle)])
+    app.add_routes(
+        [
+            web.get("/", handle),
+            web.get("/ws", websocket_handler),
+            web.get("/{name}", handle),
+        ]
+    )
     return app
